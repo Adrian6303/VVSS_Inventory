f972a347c2caf4ca09ec53ce2d1afe4ed788af4c
Adrian Schiopu <adrian.schiopu19@gmail.com>
2025-05-10 14:42:38 +0300

    module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,10 +3,12 @@ module inventory {
     requires javafx.fxml;
     requires javafx.controls;
 
-    opens inventory.model to javafx.base;
+    opens inventory.model to javafx.base, org.junit.platform.commons;
     exports inventory.model;
+
     opens inventory to javafx.fxml;
     exports inventory;
+
     opens inventory.controller to javafx.fxml;
     exports inventory.controller;
 }
